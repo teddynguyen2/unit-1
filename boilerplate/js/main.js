d92f564044620d3426a9fafcf80b595a77d4f495
teddynguyen2 <61038278+teddynguyen2@users.noreply.github.com>
2021-02-09 00:27:42 -0500

    Update main.js

--- boilerplate/js/main.js
+++ boilerplate/js/main.js
@@ -5,7 +5,6 @@ function myfunc(){
 
 window.onload = myfunc();
 
-/*
 
 //initialize function called when the script loads
 function initialize(){
@@ -16,7 +15,7 @@ function initialize(){
 function cities(){
 	//define two arrays for cities and population
 	var cityPop = [
-		{
+		{ 
 			city: 'Madison',
 			population: 233209
 		},
@@ -39,10 +38,10 @@ function cities(){
 
 	//append a header row to the table
 	$("table").append("<tr>");
-
+	
 	//add the "City" and "Population" columns to the header row
 	$("tr").append("<th>City</th><th>Population</th>");
-
+	
 	//loop to add a new row for each city
     for (var i = 0; i < cityPop.length; i++){
         //assign longer html strings to a variable
@@ -55,13 +54,15 @@ function cities(){
     addEvents();
 };
 
-//adds a city size column to the table
+//adds column, which includes different city sizes
 function addColumns(cityPop){
-    $('tr').each(function(i){
+    //adds each city size to the column
+    $("tr").each(function(i){
     	if (i == 0){
-    		$(this).append('<th>City Size</th>');
+    		$(this).append("<th>City Size</th>");
     	} else {
     		var citySize;
+
     		if (cityPop[i-1].population < 100000){
     			citySize = 'Small';
     		} else if (cityPop[i-1].population < 500000){
@@ -69,33 +70,41 @@ function addColumns(cityPop){
     		} else {
     			citySize = 'Large';
     		};
-    		$(this).append('<td>' + citySize + '</td>');
+		
+		//adds new city size
+    		$(this).append("<td>" + citySize + "</td>");
     	};
     });
 };
 
-//adds event listeners to the table
+//adds colors when mouse is over desired location
 function addEvents(){
-	//change text to random color function
-	$('table').mouseover(function(){
+	//checks if mouse is over desired location; changes colors
+	$("table").mouseover(function(){
 		var color = "rgb(";
+		
+		//loop for obtaining random colors
 		for (var i=0; i<3; i++){
 			var random = Math.round(Math.random() * 255);
 			color += random;
+
 			if (i<2){
 				color += ",";
 			} else {
 				color += ")";
-			};
+			}
 		};
+		//assigning colors
 		$(this).css('color', color);
 	});
+	
+	//pops alert message when user clicks table
 	function clickme(){
-		alert('You clicked me.');
+		alert('Hey, you clicked me!');
 	};
-	$('table').on('click', clickme);
+	
+	$("table").on('click', clickme);
 };
 
-//call the initialize function
+//document is ready and calls the initialize function
 $(document).ready(initialize);
-*/
