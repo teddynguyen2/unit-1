5f88233b821095d8fd4b5fbf63e63a0558e76b88
teddynguyen2 <61038278+teddynguyen2@users.noreply.github.com>
2021-02-08 21:50:02 -0500

    Update main.js

--- boilerplate/js/main.js
+++ boilerplate/js/main.js
@@ -1,11 +1,4 @@
 // Add all scripts to the JS folder
-function myfunc(){
-    var mydiv = document.getElementById("mydiv");
-    mydiv.innerHTML = "Hello World.";
-};
-
-window.onload = myfunc();
-
 
 //initialize function called when the script loads
 function initialize(){
